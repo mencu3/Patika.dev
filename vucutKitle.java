717c4a671ec96906072a285621a9d219a9962aef
HarunYildiz <yildizharunn@gmail.com>
2023-01-16 09:44:26 +0300

    vucutKitle endeksi

---
+++ vucutKitle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class vucutKitle {
+    public static void main(String[] args) {
+        int kg;
+        double endeks,boy;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz:");
+        boy = inp.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz:");
+        kg = inp.nextInt();
+
+        endeks = (kg / (boy * boy));
+
+        System.out.print("Vücut kitle endeksiniz:" +endeks);
+
+
+
+
+
+    }
+}
