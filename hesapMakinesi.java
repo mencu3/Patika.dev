6ea0ae0755c15a4a3695f86331d4f68137e66a9e
HarunYildiz <yildizharunn@gmail.com>
2023-01-16 14:43:47 +0300

    hesapMakinesi

---
+++ hesapMakinesi.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class hesapMakinesi {
+    public static void main(String[] args) {
+
+        int n1,n2,select;
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz:");
+        n1= input.nextInt();
+        System.out.print("İkinci sayıyı giriniz:");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz:");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1 -> System.out.println("Toplam:" + (n1 + n2));
+            case 2 -> System.out.println("Çıkarma:" + (n1 - n2));
+            case 3 -> System.out.println("Çarpma:" + (n1 * n2));
+            case 4 -> {
+                if (n2 == 0) {
+                    System.out.println("Sıfıra bölünemez!");
+                } else {
+                    System.out.println("Bölme:" + (n1 / n2));
+                }
+            }
+            default -> System.out.print("Lütfen geçerli sayı giriniz!!!");
+        }
+    }
+}
