b4c7f19365e479e42cc57201221f9c7b022ea89b
HarunYildiz <yildizharunn@gmail.com>
2023-02-28 16:38:19 +0300

    mükemmel sayi eklendi

---
+++ MukemmelSayi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        int sayi,toplam=0;
+        System.out.print("Bir sayı giriniz:");
+        sayi = input.nextInt();
+        for (int i = 1;i<sayi;i++){
+            if (sayi % i == 0){
+                toplam += i;
+            }
+        }
+        if (toplam == sayi){
+            System.out.println(sayi + ":Mükemmel Sayıdır");
+        }
+        else {
+            System.out.println(sayi + ":Mükemmel sayı değildir");
+        }
+    }
+}
