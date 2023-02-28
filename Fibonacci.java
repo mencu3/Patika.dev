aaefb386d63b311e54b964fb481f73acf7d84ccd
HarunYildiz <yildizharunn@gmail.com>
2023-02-28 16:48:45 +0300

    added fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int element, first = 0, second = 1;
+
+        System.out.print("Fibonacci serisinin eleman sayısını giriniz: ");
+        element = input.nextInt();
+
+        for (int i = 1; i <= element + 1; i++) {
+            System.out.print(first + " ");
+            int total = first + second;
+            first = second;
+            second = total;
+        }
+    }
+}
