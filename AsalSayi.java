bb3e9e8534f70d2e80d28d4b845396d4ac93bc0b
HarunYildiz <yildizharunn@gmail.com>
2023-02-28 16:45:58 +0300

    AsalSayi Eklendi

---
+++ AsalSayi.java
@@ -0,0 +1,19 @@
+public class AsalSayi {
+    public static void main(String[] args) {
+        int k, p;
+
+        for (k = 2; k <= 100; k++) {
+            int counter = 0;
+
+            for (p = 1; p <= k; p++) {
+
+                if (k % p == 0) {
+                    counter++;
+                }
+            }
+            if (counter <= 2) {
+                System.out.println(k);
+            }
+        }
+    }
+}
